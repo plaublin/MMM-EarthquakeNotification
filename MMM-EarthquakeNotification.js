8890674b5bbd103f4f58e5fb64d6eb4268ecf290
Pierre-Louis Aublin <pl@pakupaku.me>
2023-01-16 14:46:16 +0900

    update code

--- MMM-EarthquakeNotification.js
+++ MMM-EarthquakeNotification.js
@@ -2,9 +2,11 @@ Module.register("MMM-EarthquakeNotification", {
 
 	// defaut configuration values
 	defaults: {
-		urlUpdateInterval: 60, // url check interval in second
-		renderInterval: 1, // change displayed notification interval in second
-		url: "https://www.data.jma.go.jp/developer/xml/feed/eqvol.xml"
+		urlUpdateInterval: 30, // url check interval in second
+		renderInterval: 5, // change displayed notification interval in second
+		url: "https://www.data.jma.go.jp/developer/xml/feed/eqvol.xml",
+		place: "東京",
+		minShindo: 5,
 	},
 
 	// executed when the module is loaded successfully
@@ -19,20 +21,26 @@ Module.register("MMM-EarthquakeNotification", {
 		setInterval( () => {
 			this.updateDom()
 		}, this.config.renderInterval*1000)
+
+		this.sendSocketNotification("FETCH_FEED_URL", this.config.url)
 	},
 
 	// render the content on MM screen
 	// called automatically by MM core when it needs the module output to be refreshed
 	getDom: function() {
-		var notification = ["No earthquake information available for now...", ""]
+		var element = document.createElement("div")
+		//element.className = "thin large bright"
+		element.className = "thin medium bright"
+
+		//var notification = ["No earthquake information available for now...", ""]
 		if (this.notifications.length > 0) {
-			notification = this.notifications[this.currentNotification]
+			var notification = this.notifications[this.currentNotification]
 			this.currentNotification = (this.currentNotification + 1) % this.notifications.length
+			element.innerHTML = notification[0] + "<br />" + notification[1]
+		} else {
+			element.innerHTML = ""
 		}
 
-		var element = document.createElement("div")
-		element.className = "myContent"
-		element.innerHTML = notification[0] + "<br />" + notification[1]
 		return element
 	},
 
@@ -67,12 +75,15 @@ Module.register("MMM-EarthquakeNotification", {
 					// Head -> Headline -> Text 何時地震があった
 					// Body -> Earthquake -> Hypocenter -> Area -> Name
 					// Comments -> ForecastComment -> Text 津波のリスク
-					//TODO keep only the interesting ones (close to home or big intensity)
 
 					var headline = xmlDoc.getElementsByTagName("Headline")[0].childNodes[1].textContent
 					var hypocenter = xmlDoc.getElementsByTagName("Hypocenter")[0].childNodes[1].childNodes[1].textContent
+					var shindo = xmlDoc.getElementsByTagName("jmx_eb:Magnitude")[0].textContent
 					var comment = xmlDoc.getElementsByTagName("ForecastComment")[0].childNodes[1].textContent
-					this.notifications.push([hypocenter + "に、" + headline, comment])
+
+					if (hypocenter.startsWith(this.config.place) || parseFloat(shindo) > this.config.minShindo) {
+						this.notifications.push([hypocenter + "に、" + headline + "震度" + shindo, comment])
+					}
 				}
 
 				//example of 火山: kkhttps://www.data.jma.go.jp/developer/xml/data/20221214050038_0_VFVO53_010000.xml
--- node_helper.js
+++ node_helper.js
@@ -1,22 +1,30 @@
 var NodeHelper = require("node_helper")
-const fetch = require("fetch");
+const fetch = require("node-fetch");
 
 module.exports = NodeHelper.create({
 	start: function() {
-		this.countDown = 10000000
 	},
 
 	socketNotificationReceived: function(notification, payload) {
+		const nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
+		const headers = {
+			"User-Agent": "Mozilla/5.0 (Node.js " + nodeVersion + ") MagicMirror/" + global.version + " (https://github.com/MichMich/MagicMirror/)",
+			"Cache-Control": "max-age=0, no-cache, no-store, must-revalidate",
+			Pragma: "no-cache"
+		};
+
 		switch(notification) {
 			case "FETCH_FEED_URL":
-				fetch(payload)
+				//console.log("Need to fetch the feed")
+				fetch(payload, { headers: headers })
 					.then(response => response.text())
 					.then(xmlString => {
 						this.sendSocketNotification("RECEIVE_XML_FEED", xmlString)
 				})
 				break
 			case "FETCH_SINGLE_EVENT":
-				fetch(payload)
+				//console.log("Need to fetch a single event")
+				fetch(payload, { headers: headers })
 					.then(response => response.text())
 					.then(xmlString => {
 						this.sendSocketNotification("RECEIVE_SINGLE_EVENT", xmlString)
